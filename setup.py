cb1af0d236e600d616a9b3f0ceddb3203a642ca2
José Ángel González <jose.gonzalez@symanto.com>
2024-01-25 12:58:26 +0100

    add tiktoken in bedrock extra

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ DEV_DEPS = [
 EXTRAS_REQUIRES: Dict[str, List[str]] = {
     "openai": ["openai>=1", "tiktoken>=0.4.0"],
     "azure_openai": ["openai>=1", "tiktoken>=0.4.0"],
-    "bedrock": ["boto3"],
+    "bedrock": ["boto3", "tiktoken>=0.4.0"],
     "ai21": ["ai21"],
     "anthropic": ["anthropic>=0.7.2"],
     "cohere": ["cohere>=4.36"],
